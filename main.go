81ea85bed8c034a2ba101b4d1cffcba3c118b459
rakarmp <raka_088@smkassalaambandung.sch.id>
2023-09-03 21:10:38 +0700

    :ocean: ADD: Primary router all for playlist

--- main.go
+++ main.go
@@ -1,13 +1,32 @@
 package main
 
 import (
+	"example/first-web-service/controllers"
+	"example/first-web-service/models"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	// Base Gin
 	router := gin.Default()
-	router.GET("/", getWelcome)
-	router.GET("/playlist", getPlaylist)
+	// Models
+	models.ConnectDatabase()
 
+	// Router
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"status":  http.StatusOK,
+			"message": "Simple Playlist API, Pure GO",
+		})
+	})
+	router.GET("/playlist", controllers.FindPlaylist)
+	router.POST("/playlist", controllers.CreatePlaylist)
+	router.GET("/playlist/:id", controllers.FindPlaylistId)
+	router.PATCH("playlist/:id", controllers.UpdatePlaylist)
+	router.DELETE("/playlist/:id", controllers.DeletePlaylist)
+
+	// Server
 	router.Run("localhost:8000")
 }
