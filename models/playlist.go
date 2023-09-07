e6f1e0819eccae7aa80e4971cff869403a311282
rakarmp <raka_088@smkassalaambandung.sch.id>
2023-09-07 08:48:38 +0700

    ADD: Input playlist to create info song

--- models/playlist.go
+++ models/playlist.go
@@ -4,17 +4,20 @@ type Playlist struct {
 	ID     uint   `json:"id" gorm:"primary_key"`
 	Song   string `json:"song"`
 	Artist string `json:"artist"`
+	Info   string `json:"info"`
 	URL    string `json:"url"`
 }
 
 type CreatePlaylist struct {
 	Song   string `json:"song" binding:"required"`
 	Artist string `json:"artist" binding:"required"`
+	Info   string `json:"info" binding:"required"`
 	URL    string `json:"url" binding:"required"`
 }
 
 type UpdatePlaylist struct {
 	Song   string `json:"song"`
 	Artist string `json:"artist"`
+	Info   string `json:"info"`
 	URL    string `json:"url"`
 }
